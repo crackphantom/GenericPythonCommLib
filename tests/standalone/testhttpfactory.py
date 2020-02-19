cd95e505e97a667701b7489b71c4dcd6202cf270
crackphantom <crackphantom@datadorks.com>
2020-02-19 12:04:55 -0600

    Make testing more generic and output cleaner

--- tests/standalone/testhttpfactory.py
+++ tests/standalone/testhttpfactory.py
@@ -3,11 +3,21 @@ Created on Oct 23, 2019
 
 @author: crackphantom
 '''
+# Uncomment to set logging at debug
+# first thing so other loggers are properly set when loading modules etc...
+# import logging
+# logging.basicConfig(level=logging.DEBUG)
+
 import unittest
-from genericpcommlib.http import factory
+from datadorks.pcomm.http.clients import factory
+
+# address 'No handlers could be found for logger "root"' which annoys me
+# code used to address doesn't belong in the lib we're testing
+import logging
+logging.basicConfig()
+
 
 class TestHttpFactory(unittest.TestCase):
-
 
     def testBadDefaultClientForGetSync(self):
         exceptionThrown = False
@@ -21,14 +31,15 @@ class TestHttpFactory(unittest.TestCase):
     def testBadDefaultClientWithBlacklistEverythingForGetSync(self):
         exceptionThrown = False
         try:
-            client = factory.getNewSyncHttpClient(None, [factory.URLLIB2])
+            factory.SYNC_CLIENT_CLASSES.keys()
+            client = factory.getNewSyncHttpClient(None, factory.SYNC_CLIENT_CLASSES.keys())
         except Exception as e:
             exceptionThrown = True
             self.assertEquals("Unable to create a sync http client from configured library", str(e))
         self.assertTrue(exceptionThrown)
 
     def testSpecifyGoodClientWithWhiteListForGetSync(self):
-        client = factory.getNewSyncHttpClient([factory.URLLIB2])
+        client = factory.getNewSyncHttpClient(factory.SYNC_CLIENT_CLASSES.keys())
         self.assertIsNotNone(client)
 
     def testSpecifyGoodClientWithNoListsForGetSync(self):
@@ -49,5 +60,7 @@ class TestHttpFactory(unittest.TestCase):
         self.assertTrue(exceptionThrown)
 
 if __name__ == "__main__":
+    # /usr/bin/python2.7 -m unittest -v tests.standalone.testhttpfactory.TestHttpFactory
+    # /usr/bin/python3 -m unittest -v tests.standalone.testhttpfactory.TestHttpFactory
     #import sys;sys.argv = ['', 'Test.testName']
     unittest.main()
